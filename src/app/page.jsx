68d32bc65a19fc27e40933b9c152904862b39470
Matheus Couto <matheuscouto.tds2.senai@gmail.com>
2025-05-15 08:25:37 -0300

    refactor: initial configs

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Games retro</h1>
+      <p>Recordes de games retro</p>
+    </main>
+  );
+}
